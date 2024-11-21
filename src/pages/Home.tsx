5cf3d917bc5f601de2718a2539dbac860ec4a026
Vinay Maheshwari <vinaymaheshwari35@gmail.com>
2024-11-21 15:04:56 +0530

    Add: react-router-dom for routing, structure for home page i:e; RootLayout.tsx

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import RootLayout from '../layout/RootLayout'
+
+const Home = () => {
+  return (
+    <RootLayout>
+      <div>
+        Home
+      </div>
+    </RootLayout>
+  )
+}
+
+export default Home
