9aa4aa11cc74dcab8956c1166b57ea86c193b0f8
Vinay Maheshwari <vinaymaheshwari35@gmail.com>
2024-11-25 16:45:00 +0530

    Add: Brand Authority section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { appRoutes } from './utils'
-import Home from './pages/home/Home'
+import Home from './pages/Home'
 import "./App.css"
 
 const App = () => {
---
+++ src/pages/Home.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import RootLayout from "../layout/RootLayout"
+import Hero from "../components/Hero"
+import GlobalNews from "../components/GlobalNews"
+import Journals from "../components/Journals"
+import WhyChooseUs from "../components/WhyChooseUs"
+import TailorPR from "../components/TailorPR/TailorPR"
+import FAQs from "../components/FAQs"
+import BrandsAuthority from '../components/BrandsAuthority'
+
+
+const Home = () => {
+  return (
+    <RootLayout>
+      <div className=''>
+        <Hero />
+        {/* Global News Section */}
+        <GlobalNews />
+        {/* Journey Section */}
+        <Journals />
+        <WhyChooseUs/>
+        <TailorPR/>
+        <BrandsAuthority/>
+        <FAQs/>
+      </div>
+    </RootLayout>
+  )
+}
+
+export default Home
