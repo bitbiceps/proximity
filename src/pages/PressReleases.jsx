21fc953b9b85d79e6789e65013bca3ff31229d17
Sahilkadge <sahilkadgeoffical@gmail.com>
2024-12-09 16:25:37 +0530

    changes in press release page

--- src/pages/PressReleases.jsx
+++ src/pages/PressReleases.jsx
@@ -11,126 +11,54 @@ import article3 from "../assets/articles/article3.png";
 import map1 from "../assets/testing/map1.mp4";
 
 const PressReleases = () => {
-    const [openDropdown, setOpenDropdown] = useState(null); 
-    const [search, setSearch] = useState("");
-    const [chips, setChips] = useState(["Newest to Oldest", "XYZ"]);
-    const [sort, setSort] = useState([]);
-    const [channel, setChannel ] = useState([]);
-    const [year, setYear ] = useState([]);
+    const [isChannelOpen, setIsChannelOpen] = useState(false);
+    const [isYearOpen, setIsYearOpen] = useState(false);
+    const [isSortOpen, setIsSortOpen] = useState(false);
+    const [search, setSearch] = useState("Search for video/audio")
+    const [chips, setChips] = useState(["Newest to Oldest", "XYZ"])
 
     const currentYear = new Date().getFullYear();
+
     const years = Array.from({ length: 10 }, (_, i) => currentYear - i);
-    const removeSort = (indexToRemove) => {
-        setSort((prevChips) => prevChips.filter((_, idx) => idx !== indexToRemove));
-    };
-    const removeChannel = (indexToRemove) => {
-        setChannel((prevChips) => prevChips.filter((_, idx) => idx !== indexToRemove));
-    };
-    const removeYear = (indexToRemove) => {
-        setYear((prevChips) => prevChips.filter((_, idx) => idx !== indexToRemove));
-    };
+
+
     const removeChip = (indexToRemove) => {
         setChips((prevChips) => prevChips.filter((_, idx) => idx !== indexToRemove));
     };
 
-    const handleDropdownToggle = (dropdown) => {
-        setOpenDropdown((prev) => (prev === dropdown ? null : dropdown));
-    };
 
     return (
         <RootLayout>
             <HomeHero vdo={map1} label1={"Comprehensive Resources for"} label2={"Your Immigration Journey"} />
-            <div className='mt-[100px] w-full lg:w-[85vw] mx-auto'>
-                <div className='flex items-center  justify-between gap-6'>
-                    <div className='bg-app-blue-blend flex h-[60px] items-center justify-center md:w-fit rounded-full px-10 text-black gap-1 w-fit'>
-                        <AiOutlineSearch className='text-[20px]' />
-                        <input
-                            onChange={e => setSearch(e.target.value)}
-                            type="text"
-                            placeholder='Search for video/audio'
-                            className='placeholder:text-[20px] text-[20px] placeholder:text-[#000000] outline-none ring-0 w-screen lg:w-[600px] p-4  font-medium bg-transparent h-full'
-                            value={search}
-                        />
+            <div className='mt-[100px] w-full  md:w-[80vw] mx-auto'>
+                <div className=' flex items-center h-fit justify-between gap-6'>
+                    <div className='bg-app-blue-blend flex  h-[65px] items-center justify-center md:w-fit rounded-full px-10 text-black gap-1 w-screen'>
+                        <AiOutlineSearch className='text-2xl' />
+                        <input onChange={e => setSearch(e.target.value)} type="text" className='outline-none ring-0 w-screen lg:w-[600px] p-4 text-2xl font-medium  bg-transparent h-full' value={search} />
                     </div>
-                    
-                        <div className='w-fit bg-app-blue-blend h-[60px] flex items-center justify-center  rounded-full px-10 text-[18px] gap-2'>
-                            <Dropdown
-                                state={channel}
-                                setState={setChannel}
-                                isOpen={openDropdown === "channel"}
-                                setIsOpen={() => handleDropdownToggle("channel")}
-                                options={["ABP News", "Zee News", "BBC", "India tv"]}
-                                label={"Channel"}
-                            />
-                            <div className="w-[30px] rotate-90 bg-gray-400 h-[2px]"></div>
-                            <Dropdown
-                                state={year}
-                                setState={setYear}
-                                isOpen={openDropdown === "year"}
-                                setIsOpen={() => handleDropdownToggle("year")}
-                                options={years}
-                                label={"Year"}
-                            />
-                        </div>
-                    
-                   
-                    <div className=' w-fit h-[60px] flex justify-center items-center bg-app-blue-blend rounded-full px-10'>
-                        <Dropdown
-                            state={sort}
-                            setState={setSort}
-                            isOpen={openDropdown === "sort"}
-                            setIsOpen={() => handleDropdownToggle("sort")}
-                            options={["Newest to Oldest", "Oldest to Newest", "Latest first"]}
-                            label={"Sort"}
-                        />
+                    <div className='bg-app-blue-blend h-[65px] flex items-center justify-center w-fit rounded-full px-10 text-black  gap-2'>
+                        <Dropdown state={chips} setState={setChips} isOpen={isChannelOpen} setIsOpen={setIsChannelOpen} options={["ABP News", "Zee News", "BBC", "India tv"]} label={"Channel"} />
+                        <div className="w-[30px] rotate-90 bg-gray-400  h-[2px]"></div>
+                        <Dropdown state={chips} setState={setChips} isOpen={isYearOpen} setIsOpen={setIsYearOpen} options={years} label={"Year"} />
                     </div>
-                    
-                   
+                    <div className='w-fit h-[65px] flex justify-center items-center bg-app-blue-blend rounded-full px-10'>
+                        <Dropdown state={chips} setState={setChips} isOpen={isSortOpen} setIsOpen={setIsSortOpen} options={["Newest to Oldest", "Oldest to Newest", "Latest first"]} label={"Sort"} />
+                    </div>
                 </div>
 
-                <div className='w-full gap-[16px] flex h-auto'>
-                    <div className='w-[55%] '>
-                    
-                    </div>
-                    <div className='w-[30%] gap-[16px] flex flex-row '>
-                        <div className='w-[50%] flex flex-col '>
-                        {channel.map((channel, id) => (
-                            <div
-                                key={`${channel}-${id}`}
-                                className='flex w-full my-[8px] py-[8px] px-[16px] h-[40px] items-center justify-around gap-4 text-white text-[14px] font-semibold rounded-full bg-app-purple-2'
-                            >
-                                <p className='text-sm font-bold'>{channel}</p>
-                                <RxCross2 className='text-xl cursor-pointer' onClick={() => removeChannel(id)} />
-                            </div>
-                        ))}
-                        </div>
-
-                        {/* year  */}
-                        <div className='w-[50%] flex-col'>
-                        {year.map((year, id) => (
-                            <div
-                                key={`${year}-${id}`}
-                                className='flex w-full my-[8px] py-[8px] px-[16px] h-[40px] items-center justify-around gap-4 text-white text-[14px] font-semibold rounded-full bg-app-purple-2 '
-                            >
-                                <p className='text-sm font-bold'>{year}</p>
-                                <RxCross2 className='text-xl cursor-pointer' onClick={() => removeYear(id)} />
-                            </div>
-                        ))}
-                        </div>
-                    </div>
-                    <div className='w-[15%] flex flex-col'>
-                    {sort.map((sort, id) => (
-                            <div
-                                key={`${sort}-${id}`}
-                                className='flex w-full my-[8px] py-[8px] px-[16px] h-[40px] items-center justify-around gap-4 text-white text-[14px] font-semibold rounded-full bg-app-purple-2 '
-                            >
-                                <p className='text-sm font-bold'>{sort}</p>
-                                <RxCross2 className='text-xl cursor-pointer' onClick={() => removeChip(id)} />
-                            </div>
+                {chips.length > 0 && (
+                    <div className='flex flex-wrap justify-start h-auto gap-4 mt-[40px]'>
+                        {chips.map((chip, idx) => (
+                             <div
+                             key={`${chip}-${idx}`}
+                             className='flex w-auto my-[8px] py-[8px] px-[16px] h-[40px] items-center justify-around gap-4 text-white text-[14px] font-semibold rounded-full bg-app-purple-2'
+                         >
+                             <p className='text-sm font-bold'>{chip}</p>
+                             <RxCross2 className='text-xl cursor-pointer' onClick={() => removeChip(idx)} />
+                         </div> 
                         ))}
                     </div>
-
-                </div>
+                )}
 
                 <div className='mt-[50px]'>
                     <div className="grid grid-cols-3 items-center justify-evenly gap-[32px]">
@@ -187,3 +115,5 @@ const PressReleases = () => {
 };
 
 export default PressReleases;
+
+
